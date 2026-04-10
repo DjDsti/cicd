af733fbbd6e2fb9a62b37f4eeb307e48a3294b85
DjDsti <djamila.mokhtari@edu.dsti.institute>
2026-04-10 13:25:37 +0200

    Update wine_linear_regression_test.py

--- wine_linear_regression_test.py
+++ wine_linear_regression_test.py
@@ -24,13 +24,6 @@ from sklearn.linear_model import ElasticNet
 
 import logging
 
-csv_url =\
-    'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
-try:
-    data = pd.read_csv(csv_url, sep=';')
-except Exception as e:
-    logger.exception(
-    "Unable to download training & test CSV, check your internet connection. Error: %s", e)
 
 # COMMAND ----------
 
